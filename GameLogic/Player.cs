e3128098ab2ef540116474012fc0337a43f485ce
main <rybvlad4@gmail.com>
2022-03-01 23:40:11 +0300

    initial commit

---
+++ GameLogic/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLogic
+{
+    internal class Player
+    {
+        public string Name { get; set; }
+        public int Hp { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Player(string name, int hp, int x, int y) { 
+            this.Name = name;
+            this.Hp = hp;
+            this.X = x;
+            this.Y = y;
+        }
+
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System;
+
+namespace Server
+{
+	class Program
+	{
+
+		static int port = 26665;
+		//static string ip_address = "127.0.0.1";
+
+		static IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, port);
+		static Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+		static void Main(string[] args)
+		{
+			Run();
+		}
+
+		static List<TcpClient> clients = new();
+
+		/// <summary>
+		/// Main server method
+		/// </summary>
+		static void Run()
+		{
+			TcpListener listener = new(endPoint);
+			listener.Start();
+
+			// Game 
+			while (true)
+			{
+				// Потребитель выходного канала работает с ограниченной функциональностью из-за отсутствия зависимой службы: Microsoft.VisualStudio.Shell.OutputChannelStore (0.1).
+
+			}
+
+			listener.Stop();
+		}
+
+
+		static void AcceptConnections(TcpListener listener)
+		{
+			if (listener.Pending())
+			{
+				var newClient = listener.AcceptTcpClient();
+				SendServerInfo(newClient);
+
+			}
+		}
+
+		static void SendServerInfo(TcpClient client)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
