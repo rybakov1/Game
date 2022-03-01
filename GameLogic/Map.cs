62567efc010f811410f93cd8e0592c9211f7f4d6
Максим Казанцев <m.p.rybakov@gmail.com>
2022-03-02 02:57:33 +0300

    added basic map

---
+++ GameLogic/Map.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Logic
+{
+	public class Map
+	{
+		public TerrainTile[,] TerrinTiles;
+		public ObjectTile[,] ObjectTiles;
+		private int _width;
+		private int _height;
+
+		public int Height
+		{
+			get => _height; 
+			set => _height = value;
+		}
+		public int Width
+		{
+			get => _width;
+			init => _width = value;
+		}
+
+		public Map(int width, int height)
+		{
+			_width = width;
+			_height = height;
+
+			TerrinTiles = new TerrainTile[width, height];
+			TerrinTiles.Initialize();
+			ObjectTiles = new ObjectTile[width, height];
+			ObjectTiles.Initialize();
+		}
+
+		/// <summary>
+		/// Fill map for test
+		/// </summary>
+		public void Fill(TerrainTile tile)
+		{
+			for (int x = 0; x < Width; x++)
+				for (int y = 0; y < Height; y++)
+					TerrinTiles[x, y] = tile;
+		}
+	}
+}
---
+++ GameLogic/Tiles.cs
@@ -0,0 +1,18 @@
+﻿namespace Game.Logic
+{
+	public enum TerrainTile
+	{
+		Free,
+		Sand,
+		Grass,
+		Water,
+		Rock
+	}
+
+	public enum ObjectTile
+	{
+		Tree,
+		Boulder,
+		Crate
+	}
+}
