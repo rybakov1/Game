aada47d1e3d557626e579b8414d0ea1589253e29
main <rybvlad4@gmail.com>
2022-03-02 00:25:28 +0300

    Added messaging class

---
+++ GameLogic/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace GameLogic
+{
+    internal class Message
+    {
+        public float PosX { get; set; }
+        public float PosY { get; set; }
+        public int PlayerHp { get; set; }
+    }
+}
