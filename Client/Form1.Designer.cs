7a8fffe0123b3685de4860f07143ba8fbff98d09
main <rybvlad4@gmail.com>
2022-03-02 03:39:34 +0300

    playerMovements

--- Client/Form1.Designer.cs
+++ Client/Form1.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // Form1
@@ -35,6 +37,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(981, 525);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
@@ -43,5 +46,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Client/Form1.cs
+++ Client/Form1.cs
@@ -6,26 +6,52 @@ namespace Client
     public partial class Form1 : Form
     {
         public Image heroSprites;
-
+        public Entity player;
         public Form1()
         {
             InitializeComponent();
-        }
 
+            timer1.Interval = 10;
+            timer1.Tick += new EventHandler(Update);
+
+            KeyDown += new KeyEventHandler(OnPress);
+            Run();
+        }
+        public void OnPress(object sender, KeyEventArgs e) {
+
+            switch (e.KeyCode) {
+                case Keys.W:
+                    player.Move(0, -1);
+                    break;                
+                case Keys.S:
+                    player.Move(0, 1);
+                    break;                
+                case Keys.A:
+                    player.Move(-1, 0);
+                    break;                
+                case Keys.D:
+                    player.Move(1, 0);
+                    break;
+            }
+        }
 
         public void Run()
         {
-#pragma warning disable CS8602 // –азыменование веро€тной пустой ссылки.
-            heroSprites = new Bitmap(Path.Combine(new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.FullName.ToString(), "Sprites\\PlayerWalk\\walk down.png"));
-#pragma warning restore CS8602 // –азыменование веро€тной пустой ссылки.
+            heroSprites = new Bitmap("C:\\Users\\main\\source\\repos\\Game\\Client\\idle_down.jpg");
 
-            Entity player = new(100, 100, Hero.IdleFrames, Hero.WalkFrames, Hero.AttackFrames, Hero.DeathFrames, heroSprites);
+            player = new(100, 100, Hero.IdleFrames, Hero.WalkFrames, Hero.AttackFrames, Hero.DeathFrames, heroSprites);
+            timer1.Start();
+        }
+
+        public void Update(object sender, EventArgs e) 
+        {
+            Invalidate();
         }
 
         private void OnPaint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            g.DrawImage(heroSprites, new PointF(100, 100));
+            g.DrawImage(player.Sprite, new Rectangle(new Point(player.PosX, player.PosY), new Size(150, 150)), 0, 0, 150, 150, GraphicsUnit.Pixel);
         }
     }
 }
